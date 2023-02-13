55285e144c2390ed0489ff2fc20af19ea3cfcf21
Veronikataras2012 <91659198+Veronikataras2012@users.noreply.github.com>
2023-02-13 13:43:23 +0300

    Добавление функции ввода массива из консоли

--- Program.cs
+++ Program.cs
@@ -3,4 +3,15 @@ Console.WriteLine($"Введите {N} значений массива ");
 string?[] array= GetArrayConsole(N);
 //Заполняем массив значениями строк
 Console.WriteLine($"Введите значения массива ");
+string?[] GetArrayConsole(int n)
+
+{string?[] res = new string?[n];
+for (int i=0; i<10; i++)
+{
+       res[i] = Console.ReadLine();
+}
+return res;
+
+}
+
 
