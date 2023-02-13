2f525b55cab4752c165c2b2830191c292e15e946
Veronikataras2012 <91659198+Veronikataras2012@users.noreply.github.com>
2023-02-13 13:25:16 +0300

    вызываем функцию ввода массива из консоли

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿int N=10;
+Console.WriteLine($"Введите {N} значений массива ");
+string?[] array= GetArrayConsole(N);
+//Заполняем массив значениями строк
+Console.WriteLine($"Введите значения массива ");
+
